b8ddd48df3ef731993f954bc9aab2b91bcafe6e7
Christoffer Back <cback@redhat.com>
2026-03-31 22:43:29 +0200

    Enhance GPU and WiFi power management in script.sh
    Added NVIDIA GPU performance tuning commands for better power management.

--- script.sh
+++ script.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-#This script is run with bare-metal-gaming.conf and will tune GPU and Intel WiFi. Worth it for laptops
-
 case "$1" in
     start)
         # Force NVIDIA Max Performance
         /usr/bin/nvidia-settings -a "[gpu:0]/GPUPowerMizerMode=1" > /dev/null 2>&1
         
+        # Force NVIDIA GPU to Maximum Performance Mode
+        nvidia-smi -pm 1 # Enable Persistence Mode
+        
+        # MSI GE66 3070 usually caps around 125W-130W, sometimes 140W. 
+        # Setting 140 is fine; the card will just take its physical max. - For now it seems to go as high as 101W with a laptop
+        nvidia-smi -pl 140 
+        # If you ever get thermal throttling, you can use --lock-gpu-clocks=1100,1650 
+
         # Force WiFi Power Management OFF
         echo 2 > /sys/class/net/wlan0/device/power_management
         /usr/sbin/iw dev wlan0 set power_save off
-        
+
         # Force Intel WiFi Driver Power Scheme
         echo 0 > /sys/module/iwlwifi/parameters/power_save
         echo 1 > /sys/module/iwlmvm/parameters/power_scheme
         ;;
 esac
-
 exit 0
